11cf16528dc7393093e8bed9f395a43755735177
Abror06 <asqarovdanater@gmail.com>
2025-11-24 23:28:55 +0500

    some changes

--- src/main/java/com/example/learningspringboot/mapper/CategoryMapper.java
+++ src/main/java/com/example/learningspringboot/mapper/CategoryMapper.java
@@ -3,13 +3,10 @@ package com.example.learningspringboot.mapper;
 import com.example.learningspringboot.dto.CategoryCreateDto;
 import com.example.learningspringboot.dto.CategoryDto;
 import com.example.learningspringboot.model.Category;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
+
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 @Component
 public interface CategoryMapper {
     CategoryDto toDto(Category category);
